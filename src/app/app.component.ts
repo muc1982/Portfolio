420f2b4ca1536dca69440747239cc7a0c6a21376
Yasin <yasin.sun@t-online.de>
2025-06-08 11:25:15 +0200

    Bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {TranslateModule} from "@ngx-translate/core";
-import {TranslateService} from "@ngx-translate/core";
+import { TranslateModule } from "@ngx-translate/core";
+import { TranslateService } from "@ngx-translate/core";
 import { RouterOutlet } from '@angular/router';
 import { Router, NavigationEnd } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
@@ -9,32 +9,58 @@ import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ FormsModule, TranslateModule, RouterOutlet],
+  imports: [FormsModule, TranslateModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   currentLang: string = "";
 
-  constructor(private translate: TranslateService, private router: Router,  @Inject(DOCUMENT) private document: Document) {
+  constructor(
+    private translate: TranslateService, 
+    private router: Router,  
+    @Inject(DOCUMENT) private document: Document
+  ) {
     this.translate.addLangs(['de', 'en']);
+    this.initializeLanguage();
+  }
 
-    let storedLang = localStorage.getItem('currentLange');
-    if (storedLang) {
-      this.setLang(storedLang);
-    } else {
+  private initializeLanguage(): void {
+    try {
+
+      if (typeof Storage !== 'undefined' && localStorage) {
+        const storedLang = localStorage.getItem('currentLang');
+        if (storedLang && (storedLang === 'de' || storedLang === 'en')) {
+          this.setLang(storedLang);
+        } else {
+          this.setLang('de');
+        }
+      } else {
+
+        this.setLang('de');
+      }
+    } catch (error) {
+      console.warn('LocalStorage not available, using default language:', error);
       this.setLang('de');
     }
   }
 
-  setLang(l: string) {
-    this.translate.setDefaultLang(l);
-    this.currentLang = l;
-    this.translate.use(l);
+  setLang(l: string): void {
+    if (l === 'de' || l === 'en') {
+      this.translate.setDefaultLang(l);
+      this.currentLang = l;
+      this.translate.use(l);
+      
+      try {
+        if (typeof Storage !== 'undefined' && localStorage) {
+          localStorage.setItem('currentLang', l);
+        }
+      } catch (error) {
+        console.warn('Could not save language preference:', error);
+      }
+    }
   }
 
   ngOnInit(): void {
-    
   }
-
 }
