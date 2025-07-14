e018663f4987ef96827879fe362c30d4c61859aa
Yasin <yasin.sun@t-online.de>
2025-07-14 22:32:59 +0200

    Bugix Footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import {TranslateModule} from "@ngx-translate/core";
-import {RouterLink } from '@angular/router';
-import { FooterMobileComponent } from "../footer-mobile/footer-mobile.component";
+import { TranslateModule } from "@ngx-translate/core";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule, RouterLink, FooterMobileComponent],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
